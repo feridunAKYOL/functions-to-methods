3c48b0cc73fb449476c89d2c2c4fbdc3dfa0bea8
feridun <59169551+feridunAKYOL@users.noreply.github.com>
2020-03-23 14:10:32 +0100

    Update review.js

--- review.js
+++ review.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  user: 'hackyourfuturebelgium',
+  user: 'feridunAKYOL',
   repo: 'functions-to-methods',
   root: process.argv[2] || './',
   ignore: ['.git', 'review.js', 'node_modules'],
